a2538531b40e2815cb69cdc806fa7dcbe9643389
dreamkinRU <12631738+dreamkinRU@users.noreply.github.com>
2018-12-22 00:49:53 +0300

    restore

---
+++ scripts/drm.js
@@ -0,0 +1,12 @@
+ $('.brands').slick({ 	
+ 	autoplay: true,
+	autoplaySpeed: 1000,
+	speed: 1000,
+	// centerMode: true,
+	// centerPadding: '20px',
+	// dots: true,
+	arrows: false,
+	slidesToShow: 6,
+	slidesToScroll: 2,
+	// fade: true,
+ });
